57c80d30c2dc53c4a480151660b7262b5725bded
Emilio <emistien@gmail.com>
2026-02-06 11:10:05 +0100

    implement ReaderAutomata

--- nio/src/babystep2/ReaderAutomata.java
+++ nio/src/babystep2/ReaderAutomata.java
@@ -12,25 +12,45 @@ public class ReaderAutomata {
 
 	private State state = State.READING_LENGTH;
 	private ByteBuffer bb;
-	
-	
-	public void handleRead(SocketChannel sc) throws IOException {
+	private byte[] data;
+
+	public byte[] handleRead(SocketChannel sc) throws IOException {
 
 		if (state == State.READING_LENGTH) {
 			
-			int length = bb.getInt();
-			// if the four bytes composing the length have been read
-			state = State.READING_MSG;
-		}
-		else if (state == State.READING_MSG) {
+			//on alloue le buffer à 4 octets pour lire la taille du message
+			bb = ByteBuffer.allocate(4);
 			sc.read(bb);
-			
-			//if all bytes composing the msg have been read
-			if(bb.remaining() == 0) {
-				
-				state = State.READING_LENGTH;
+			if(bb.hasRemaining()) {
+				return null;
 			}
+			
+			bb.rewind();
+			
+			int length = bb.getInt();
+			bb = ByteBuffer.allocate(length);
+			// if the four bytes composing the length have been read
+				//allocate a buffer to read the msg
+	
+			state = State.READING_MSG;
+			
+			
+		} else if (state == State.READING_MSG) {
+			sc.read(bb);
+			if(bb.hasRemaining()) {
+				return null;
+			}
+			
+			//on set la position à 0 pour lire les octets au debut.
+			bb.rewind();
+			data = new byte[bb.limit()];
+			bb.get(data);
+			state = State.READING_LENGTH;
+			bb=null;
+			return data;
+			
 		}
+		return null;
 	}
 
 }
--- nio/src/babystep2/WriterAutomata.java
+++ nio/src/babystep2/WriterAutomata.java
@@ -1,5 +1,23 @@
 package babystep2;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
 public class WriterAutomata {
 
+	enum State {
+		WRITING_LENGTH, WRITING_MSG, WRITING_IDLE
+	};
+	
+	private ByteBuffer bb;
+	private State state = State.WRITING_IDLE;
+	
+	void sendMsg(byte[] msg){
+		
+	}
+	
+	void handleWrite(SocketChannel sc) throws IOException {
+		sc.write(bb);
+	}
 }
